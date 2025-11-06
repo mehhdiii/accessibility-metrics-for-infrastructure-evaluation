5b83a6cad43fb0673b0ad969e45a1451b01f05f0
mehhdiii <mk05198@st.habib.edu.pk>
2025-11-06 04:30:50 +0100

    feat: add algo build file for easy access

---
+++ point-cloud-infra/StairwayDetection/build/CMakeFiles/stair_det.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/data/point-cloud-infra/StairwayDetection/src/StairVector.cpp" "/data/point-cloud-infra/StairwayDetection/build/CMakeFiles/stair_det.dir/src/StairVector.cpp.o"
+  "/data/point-cloud-infra/StairwayDetection/src/Stairs.cpp" "/data/point-cloud-infra/StairwayDetection/build/CMakeFiles/stair_det.dir/src/Stairs.cpp.o"
+  "/data/point-cloud-infra/StairwayDetection/src/planeshape.cpp" "/data/point-cloud-infra/StairwayDetection/build/CMakeFiles/stair_det.dir/src/planeshape.cpp.o"
+  "/data/point-cloud-infra/StairwayDetection/src/preanalysis.cpp" "/data/point-cloud-infra/StairwayDetection/build/CMakeFiles/stair_det.dir/src/preanalysis.cpp.o"
+  "/data/point-cloud-infra/StairwayDetection/src/recognition.cpp" "/data/point-cloud-infra/StairwayDetection/build/CMakeFiles/stair_det.dir/src/recognition.cpp.o"
+  "/data/point-cloud-infra/StairwayDetection/src/regiongrowing.cpp" "/data/point-cloud-infra/StairwayDetection/build/CMakeFiles/stair_det.dir/src/regiongrowing.cpp.o"
+  "/data/point-cloud-infra/StairwayDetection/src/regions.cpp" "/data/point-cloud-infra/StairwayDetection/build/CMakeFiles/stair_det.dir/src/regions.cpp.o"
+  "/data/point-cloud-infra/StairwayDetection/src/saveToDatabase.cpp" "/data/point-cloud-infra/StairwayDetection/build/CMakeFiles/stair_det.dir/src/saveToDatabase.cpp.o"
+  "/data/point-cloud-infra/StairwayDetection/src/segmentPatch.cpp" "/data/point-cloud-infra/StairwayDetection/build/CMakeFiles/stair_det.dir/src/segmentPatch.cpp.o"
+  "/data/point-cloud-infra/StairwayDetection/src/splitmerge.cpp" "/data/point-cloud-infra/StairwayDetection/build/CMakeFiles/stair_det.dir/src/splitmerge.cpp.o"
+  "/data/point-cloud-infra/StairwayDetection/src/stairDetectionMain.cpp" "/data/point-cloud-infra/StairwayDetection/build/CMakeFiles/stair_det.dir/src/stairDetectionMain.cpp.o"
+  "/data/point-cloud-infra/StairwayDetection/src/voxSAC.cpp" "/data/point-cloud-infra/StairwayDetection/build/CMakeFiles/stair_det.dir/src/voxSAC.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DISABLE_LIBUSB_1_0"
+  "DISABLE_OPENNI"
+  "DISABLE_OPENNI2"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "qh_QHpointer"
+  "vtkRenderingContext2D_AUTOINIT=1(vtkRenderingContextOpenGL2)"
+  "vtkRenderingCore_AUTOINIT=3(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingOpenGL2)"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/include/vtk-7.1"
+  "/usr/include/freetype2"
+  "/usr/include/pcl-1.10"
+  "/usr/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ point-cloud-infra/StairwayDetection/build/CMakeFiles/stair_det.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stair_det.dir/src/StairVector.cpp.o"
+  "CMakeFiles/stair_det.dir/src/Stairs.cpp.o"
+  "CMakeFiles/stair_det.dir/src/planeshape.cpp.o"
+  "CMakeFiles/stair_det.dir/src/preanalysis.cpp.o"
+  "CMakeFiles/stair_det.dir/src/recognition.cpp.o"
+  "CMakeFiles/stair_det.dir/src/regiongrowing.cpp.o"
+  "CMakeFiles/stair_det.dir/src/regions.cpp.o"
+  "CMakeFiles/stair_det.dir/src/saveToDatabase.cpp.o"
+  "CMakeFiles/stair_det.dir/src/segmentPatch.cpp.o"
+  "CMakeFiles/stair_det.dir/src/splitmerge.cpp.o"
+  "CMakeFiles/stair_det.dir/src/stairDetectionMain.cpp.o"
+  "CMakeFiles/stair_det.dir/src/voxSAC.cpp.o"
+  "stair_det"
+  "stair_det.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stair_det.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
